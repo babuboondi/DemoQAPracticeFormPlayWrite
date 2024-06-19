8140b7b657e1d7c2c5f29b7bc173e4fbd9318782
unknown <arshinooreen.6@gmail.com>
2024-06-19 17:35:50 +0200

    demoqacommit

---
+++ page/Login.page.ts
@@ -0,0 +1,102 @@
+import { Page } from "@playwright/test";
+const { expect } = require("@playwright/test");
+import * as selectors from '../utils/selectors.json';
+
+export default class LoginPage {
+
+    constructor(public page: Page) {
+       
+    }
+
+    async Enterfname(username: string) {
+        await this.page.locator(selectors.LoginPage.fname).type(username);
+    }
+    async Enterlname(username: string) {
+        await this.page.locator(selectors.LoginPage.lname).type(username);
+    }
+    async Enteremail(username: string) {
+        await this.page.locator(selectors.LoginPage.email).type(username);
+    }
+    async enterMobile(username: string) {
+        await this.page.locator(selectors.LoginPage.mobile).type(username);
+    }
+    async enterDOB(username: string) {
+        //await this.page.locator(selectors.LoginPage.dob).clear();
+        await this.page.locator(selectors.LoginPage.dob).type(username);
+    }
+    async enterSubject(username: string) {
+        
+        await this.page.locator(selectors.LoginPage.subject).type("English");
+        await this.page.keyboard.press('Enter');
+        const options = this.page.locator("div[class='subjects-auto-complete_menu']")
+        const values = await options.allTextContents();
+        console.log(values)
+        const optionsCount = await options.count();
+       
+        // Iterate through each value 
+        for(let i = 0; i < optionsCount; i++)
+        {
+            // Get text of nth value
+            const text = await options.nth(i).textContent();
+            console.log(text);
+            // if text matches the expected value
+            if(text==="Maths")
+            {
+                // click and break lloop
+                await options.nth(i).click();
+                break;
+            }    
+        }
+
+         
+    }
+    
+    async enterAddress(username: string) {
+        await this.page.locator(selectors.LoginPage.address).type(username);
+    }
+
+
+    async clickGender() {
+        await this.page.click(selectors.LoginPage.gender);
+    }
+    async clicksubmit() {
+        await this.page.click(selectors.LoginPage.submit);
+    }
+    async clickHobbies() {
+        await this.page.click(selectors.LoginPage.hobies);
+    }
+    async clickState(state: string) {
+        await this.page.click(selectors.LoginPage.state);
+        await this.page.waitForTimeout(3000);
+        this.page.locator('//*[contains(text(),"NCR")]').click();
+          
+        
+    }
+    async clickCity(city: string) {
+        await this.page.waitForTimeout(3000);
+        await this.page.click(selectors.LoginPage.city);
+        await this.page.waitForTimeout(3000);
+        this.page.locator('//*[contains(text(),"Delhi")]').click();
+          
+    }
+    async uplaodpic() {
+      // Click input[name="file-upload"]
+    await this.page.locator('input[id="uploadPicture"]').click();
+ 
+    // Upload fixture.pdf
+    await this.page.locator('input[id="uploadPicture"]').setInputFiles('./test/fixture.pdf');
+ 
+    // Click text=fixture.pdf
+    //await this.page.locator('text=fixture.pdf').click();
+    }
+    async screenshot()
+    {
+        await this.page.screenshot({path: 'test/visibleWindow.png'})
+    }
+    async AssertionValue()
+    {
+         
+        await expect.soft(this.page.getByText('Thanks for submitting the form'), 'Thanks for submitting the form should be visible').toBeVisible();   
+    }
+
+}
---
+++ test/customReport.ts
@@ -0,0 +1,28 @@
+import type { Stats } from './types';
+
+export default class customReport {
+  stats: Stats;
+
+  constructor(stats) {
+    this.stats = stats;
+  }
+
+  templateReport(): string {
+    return (
+      // eslint-disable-next-line indent
+      `Total Tests in Suite: ${this.stats.testsInSuite},
+Total Tests Completed: ${this.stats.totalTestsRun},
+Tests Passed: ${this.stats.expectedResults},
+Tests Failed: ${this.stats.unexpectedResults},
+Flaky Tests: ${this.stats.flakyTests},
+Test Skipped: ${this.stats.testMarkedSkipped},
+Test run was failure free? ${this.stats.failureFree},
+Duration of CPU usage in ms: ${this.stats.durationCPU},
+Duration of entire test run in ms: ${this.stats.durationSuite},
+Average Test Duration in ms: ${this.stats.avgTestDuration},
+Test Suite Duration: ${this.stats.formattedDurationSuite},
+Average Test Duration: ${this.stats.formattedAvgTestDuration},
+Number of workers used for test run: ${this.stats.workers}`
+    );
+  }
+}
